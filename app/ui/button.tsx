a5e55081c47f3f449d7383ab6261d3d9fb0d00c8
sagar_patel <sagar.bcpatel@gmail.com>
2024-02-11 16:29:56 +0530

    final commit

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
 
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
@@ -17,3 +18,4 @@ export function Button({ children, className, ...rest }: ButtonProps) {
     </button>
   );
 }
+
